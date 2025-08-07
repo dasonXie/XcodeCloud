90d1e9dc7034bd1a0188a3704d30280353e1ed3c
7320 <7320@hujing.co>
2025-08-07 16:43:51 +0800

    init

---
+++ test_XcodeCloud/test_XcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_XcodeCloudApp.swift
+//  test_XcodeCloud
+//
+//  Created by JL on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct test_XcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
